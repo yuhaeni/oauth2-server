39a989da222f9bb4e4675714d33442c8af28ada4
haeni <haeni.you@aizen.co>
2024-07-10 13:47:15 +0900

    feat(client): egisteredClientRepository 구현

--- src/main/java/com/example/oauth2server/client/domain/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.oauth2server.client.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, String> {
-
-    Client findByClientEmail(String clientEmail);
-
-}
